a2b5715315b53466be53a12e39b7c839656de681
nnouran <96547311+nouranMo@users.noreply.github.com>
2023-10-18 01:06:08 +0400

    signup in its basic form

--- public/js/validations.js
+++ public/js/validations.js
@@ -32,3 +32,73 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get references to the form and form elements
+    const signupForm = document.getElementById("form-main"); // Changed the form ID to class
+    const firstnameInput = document.getElementById("firstname");
+    const lastnameInput = document.getElementById("lastname");
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const firstnameError = document.getElementById("firstname-error");
+    const lastnameError = document.getElementById("lastname-error");
+    const emailError = document.getElementById("email-error");
+    const passwordError = document.getElementById("password-error");
+
+    // Add an event listener for the form submission
+    signupForm.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent the form from actually submitting
+
+        // Get the values of the input fields
+        const firstnameValue = firstnameInput.value;
+        const lastnameValue = lastnameInput.value;
+        const emailValue = emailInput.value;
+        const passwordValue = passwordInput.value;
+
+        // Check if the Firstname is empty
+        if (firstnameValue === "") {
+            firstnameInput.classList.add("error");
+            firstnameError.textContent = "Firstname is required";
+        } else {
+            firstnameInput.classList.remove("error");
+            firstnameError.textContent = "";
+        }
+
+        // Check if the Lastname is empty
+        if (lastnameValue === "") {
+            lastnameInput.classList.add("error");
+            lastnameError.textContent = "Lastname is required";
+        } else {
+            lastnameInput.classList.remove("error");
+            lastnameError.textContent = "";
+        }
+
+        // Check if the Email is empty and validate its format (basic email validation)
+        if (emailValue === "") {
+            emailInput.classList.add("error");
+            emailError.textContent = "Email is required";
+        } else if (!isValidEmail(emailValue)) {
+            emailInput.classList.add("error");
+            emailError.textContent = "Please enter a valid email address";
+        } else {
+            emailInput.classList.remove("error");
+            emailError.textContent = "";
+        }
+
+        // Check if the Password is empty
+        if (passwordValue === "") {
+            passwordInput.classList.add("error");
+            passwordError.textContent = "Password is required";
+        } else {
+            passwordInput.classList.remove("error");
+            passwordError.textContent = "";
+        }
+    });
+
+    // Function to validate email format
+    function isValidEmail(email) {
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailPattern.test(email);
+    }
+});
