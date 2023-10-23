1b4f2864d797f2cfdb37d262896226f23ec83ed8
dodo <nader.amir02@gmail.com>
2023-10-23 15:41:39 +0300

    Part of homepage - slideshow not perfect

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('login.ejs');
-    });
+});
+
+app.get('/homepage', (req, res) => {
+    res.render('homepage.ejs');
+});
 
  app.get('/signup', (req, res) => {
         res.render('signup.ejs');
-        });
+  });
+  
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
