1377e16d45c3ba600cc0c97ca6430e9de71b08e4
George Aziz <123029529+GeorgeAyy@users.noreply.github.com>
2023-10-25 17:27:02 +0300

    added database connection and product list page

--- app.js
+++ app.js
@@ -1,25 +1,66 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mysql = require("mysql2");
 const port = 3010; // Specify the port you want to use
 
+const connection = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "qanaa",
+  port: 3306,
+});
+connection.getConnection((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL: " + err.stack);
+    return;
+  }
+  console.log("Connected to MySQL as ID " + connection);
+});
 // Serve static files (e.g., HTML, CSS, JavaScript) from a directory
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.render('Homepage.ejs');
-  });
-
-app.get('/login', (req, res) => {
-    res.render('login.ejs');
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("Homepage.ejs");
 });
 
-app.get('/homepage', (req, res) => {
-    res.render('homepage.ejs');
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
 });
 
- app.get('/signup', (req, res) => {
-        res.render('signup.ejs');
+app.get("/homepage", (req, res) => {
+  res.render("homepage.ejs");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup.ejs");
+});
+
+const category = "Sample Category";
+const products = [
+  {
+    name: "Sample Product 1",
+    price: 10.0,
+    description: "This is a sample product.",
+    image: "/images/banner1.png",
+  },
+  {
+    name: "Sample Product 2",
+    price: 20.0,
+    description:
+      "Another sample product description sadsa das dasd asd asd sad sa das sda.",
+    image: "/images/featured2.png",
+  },
+  // Add more sample products here
+];
+
+app.get("/productList", (req, res) => {
+  res.render("productList.ejs", {
+    category,
+    products,
+    totalPages: 1,
+    currentPage: 1,
   });
-  
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
