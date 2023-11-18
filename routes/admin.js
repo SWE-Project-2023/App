d52af7ca8bbff35d7e700afe21d407855cc0d802
George Aziz <123029529+GeorgeAyy@users.noreply.github.com>
2023-11-18 22:38:11 +0200

    fixed a whoopsieee

--- routes/admin.js
+++ routes/admin.js
@@ -3,69 +3,44 @@ const router = express.Router();
 
 import express from "express";
 import multer from "multer";
-import productController from "../controllers/productController.js";
 import fs from "fs";
 const storage = multer.diskStorage({
   destination: "public/images",
   filename: (req, file, cb) => {
     cb(null, file.originalname); // Use the original filename
   },
-
+});
 import itemsController from "../controllers/itemsController.js";
-
 
 const upload = multer({ storage });
 router.get("/", function (req, res, next) {
   {
-    res.render("admin/dashboard", {
-      user: req.session.user === undefined ? "" : req.session.user,
-    });
+    res.render("admin/dashboard", {user: req.session.user===undefined?"":req.session.user});
   }
 });
 router.get("/login", function (req, res, next) {
   {
-    res.render("admin/login.ejs", {
-      user: req.session.user === undefined ? "" : req.session.user,
-    });
+    res.render("admin/login.ejs",{user: req.session.user===undefined?"":req.session.user});
   }
 });
 router.get("/dashboard", function (req, res, next) {
   {
-    res.render("admin/dashboard.ejs", {
-      user: req.session.user === undefined ? "" : req.session.user,
-    });
+    res.render("admin/dashboard.ejs",{user: req.session.user===undefined?"":req.session.user});
   }
 });
-router.get("/products", async function (req, res, next) {
-  var products = await productController.searchItems({
-    item_id: req.query.id,
-    item_title: req.query.title,
-    item_brand: req.query.brand,
-    item_cat: req.query.category,
-    item_price_min: req.query.price_min,
-    item_price_max: req.query.price_max,
-    item_qty_min: req.query.qty_min,
-    item_qty_max: req.query.qty_max,
-  });
+router.get("/products", function (req, res, next) {
   {
-    res.render("admin/products.ejs", {
-      user: req.session.user === undefined ? "" : req.session.user,
-      products,
-    });
+    res.render("admin/products.ejs",{user: req.session.user===undefined?"":req.session.user});
   }
 });
 router.get("/users", function (req, res, next) {
   {
-    res.render("admin/users.ejs", {
-      user: req.session.user === undefined ? "" : req.session.user,
-    });
+    res.render("admin/users.ejs",{user: req.session.user===undefined?"":req.session.user});
   }
 });
 router.get("/orders", function (req, res, next) {
   {
-    res.render("admin/orders.ejs", {
-      user: req.session.user === undefined ? "" : req.session.user,
-    });
+    res.render("admin/orders.ejs",{user: req.session.user===undefined?"":req.session.user});
   }
 });
 router.post("/addItem", itemsController.createItem);
@@ -94,6 +69,6 @@ router.post('/delete', (req, res) => {
   }
 });
 
-//export default
+//export default 
 
 export default router;
