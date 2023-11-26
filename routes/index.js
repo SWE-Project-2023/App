19af2a0744f3c366eb4ca4d723f81e021d7405a9
nnouran <96547311+nouranMo@users.noreply.github.com>
2023-11-27 03:10:13 +0400

    started in the backend if itempage and homepage is dynamic with database

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 import express from "express";
+import productController from "../controllers/productController.js";
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index',{user: req.session.user===undefined?"":req.session.user});
+router.get('/', async function(req, res, next) {
+  var products = await productController.searchItems({
+    undefined
+  });
+
+  res.render('index',{user: req.session.user===undefined?"":req.session.user,products});
 });
 export default router;
