41577b5c22d30b94375ad5509125e8de32d999b4
dodo <nader.amir02@gmail.com>
2023-12-19 17:56:22 +0200

    profile (still not complete) and some style changes

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ import adminRouter from "./routes/admin.js";
 import checkoutRouter from "./routes/checkout.js";
 import aboutRouter from "./routes/about.js";
 import contactRouter from "./routes/contact.js";
+import accountRouter from "./routes/account.js";
 
 // Register route handlers
 app.use("/", indexRouter);
@@ -71,6 +72,7 @@ app.use("/admin", adminRouter);
 app.use("/checkout", checkoutRouter);
 app.use("/about", aboutRouter);
 app.use("/contact", contactRouter);
+app.use("/account", accountRouter);
 // Handle logout
 app.get("/logout", (req, res) => {
   req.session.destroy();
