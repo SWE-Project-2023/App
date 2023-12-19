7a8581b4911763b4babd494024087737162de01e
Hoss <ahmed2104740@miuegypt.edu.eg>
2023-12-20 00:22:38 +0200

    fix sql

--- QanaaPharm.sql
+++ QanaaPharm.sql
@@ -75,16 +75,15 @@ CREATE TABLE `item_images` (
 --
 -- Table structure for table `orders`
 --
+SET SQL_MODE = "ALLOW_INVALID_DATES";
 
 CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `order_quantity` int(3) NOT NULL,
-  `order_date` date NOT NULL DEFAULT current_timestamp(),
-  `order_status` tinyint(1) NOT NULL DEFAULT 0,
-  `item_price` INT NOT NULL
+  `order_date` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+  `order_status` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 --
 -- Dumping data for table `orders`
@@ -108,6 +107,8 @@ CREATE TABLE `user` (
   `user_address` text NOT NULL, 
   `user_isAdmin` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
 
 --
 -- Dumping data for table `user`
@@ -133,7 +134,6 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`order_id`),
-  ADD KEY `item_id` (`item_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
@@ -184,8 +184,7 @@ ALTER TABLE `user`
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 COMMIT;
 CREATE TABLE `cart` (
   `cart_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -226,6 +225,30 @@ INSERT INTO `categories` (`category_name`) VALUES
 ('Specialty Health'),
 ('Medical Devices'),
 ('Pet Health');
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+CREATE TABLE `order_items` (
+  `order_item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `item_quantity` int(3) NOT NULL,
+  PRIMARY KEY (`order_item_id`),
+  KEY `item_id` (`item_id`),
+  CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE,
+  CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `notifications` (
+  `notification_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `notification_text` varchar(250) NOT NULL,
+  `notification_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`notification_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE locations (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -233,9 +256,6 @@ CREATE TABLE locations (
     address VARCHAR(255),
     google_maps_url VARCHAR(255)
 );
+
 INSERT INTO locations (branch_name, address, google_maps_url) 
 VALUES ('Nasr City', 'Al Manteqah Al Oula, Nasr City, Cairo Governorate 4450115', 'https://maps.app.goo.gl/gmTuJGybmFszvsUg9');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
